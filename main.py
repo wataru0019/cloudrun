6e91c6bfb02506c7e34f5071b15b0d7cab6ae5b9
柏原　亘 <watzu@kashiwabarawatarunoMacBook-puro.local>
2024-12-30 15:29:44 +0900

    add data

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
   return "Hello, this is a test deployment!"
+
+@app.route("/data")
+def data():
+  data = {
+    name: "Wataru",
+    id: "15",
+    age: "33",
+    _from: "JP"
+  }
 
 if __name__ == "__main__":
   app.run(debug=True)
